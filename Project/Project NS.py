11ffb91447c8149be5eda7daf4b4481c71b32caa
Tim <t.hoogeland@live.com>
2016-11-05 14:38:00 +0100

    werkt

--- Project/Project NS.py
+++ Project/Project NS.py
@@ -10,8 +10,6 @@ import xmltodict
 auth_details = ('t.hoogeland@live.com','3leOcnJcxNX8Hw3gJ7v5zn3EP2T_R_gEwcotJ-aQ0zzNz4ayWNO-tA')
 stations_url = 'http://webservices.ns.nl/ns-api-stations-v2'
 stations_response = requests.get(stations_url, auth=auth_details)
-api_url = 'http://webservices.ns.nl/ns-api-avt?station=%27s-Hertogenbosch'
-response = requests.get(api_url, auth=auth_details)
 stationsXML = xmltodict.parse(stations_response.text)
 beginurl='http://webservices.ns.nl/ns-api-avt?station='
 #######################################################################################################
@@ -24,21 +22,21 @@ treintype=''
 traject=''
 vertraging=''
 spoor=int
-vertrekXML = xmltodict.parse(response.text)
 stationslijst=[]
+codes=[]
+afgekort=0
 ########################################################################################################
 #writers to xml
 ########################################################################################################
 with open('stations.xml', 'w',encoding="utf8") as myXMLFile:
     myXMLFile.write(stations_response.text)
-with open('vertrektijden.xml', 'w',encoding="utf8") as myXMLFile:
-    myXMLFile.write(response.text)
 ########################################################################################################
 #Maakt een lijst van alle stations met afkortingen en synoniemen
 ########################################################################################################
 def station_lijst():
     for station in stationsXML['Stations']['Station']:
         stationslijst.append(station['Code'])
+        codes.append(station['Code'])
         stationslijst.append(station['Namen']['Kort'])
         stationslijst.append(station['Namen']['Middel'])
         stationslijst.append(station['Namen']['Lang'])
@@ -63,6 +61,10 @@ def stationskeuze():
             break
         else:
             print('Station onbekend, probeer het opnieuw.')
+    if keuze in codes:
+        keuze= stationslijst[stationslijst.index(keuze)+3]
+        global afgekort
+        afgekort=1
     keuze2=keuze.replace(" " ,"+")
     keuze3=keuze2.replace("'","%27")
     global url2
@@ -71,7 +73,16 @@ def stationskeuze():
 ##########################################################################################################
 #Real time vertrekinfo voor gekozen station
 ##########################################################################################################
+
+
 def treininfo():
+    response = requests.get(url2, auth=auth_details)
+    with open('vertrektijden.xml', 'w',encoding="utf8") as myXMLFile:
+        myXMLFile.write(response.text)
+    vertrekXML = xmltodict.parse(response.text)
+
+    print('De volgende treinen vertrekken komend uur vanaf '+keuze+':\n')
+
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
         eindbestemming = vertrek['EindBestemming']
         vertrektijd = vertrek['VertrekTijd']
@@ -90,10 +101,11 @@ def treininfo():
         except KeyError:
             vertraging2=''
 
-        print('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:5}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+ traject+'.'+'\n'+vertraging2)
+        print('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:6}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+ traject+'.'+'\n'+vertraging2)
 ############################################################################################################
 #Uit te voeren programma's
 ############################################################################################################
+station_lijst()
 stationskeuze()
 treininfo()
-
+print(url2)
