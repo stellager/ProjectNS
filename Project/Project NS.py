ffd74f61ff0cd4f1face8639633ddad309a03bc9
Tim <t.hoogeland@live.com>
2016-11-07 12:56:16 +0100

    werkt

--- Project/Project NS.py
+++ Project/Project NS.py
@@ -4,6 +4,76 @@
 #######################################################################################################
 import requests
 import xmltodict
+from tkinter import *
+root = Tk()
+
+def invoer():
+
+    global keuze
+    listbox.delete(0,END)
+    global keuze
+    keuze=''
+    keuze=entry.get()
+    keuzestring=keuze
+    station_lijst()
+    stationskeuze()
+    if onbekend==2:
+        listbox.insert(0,'Station onbekend, probeer het opnieuw.')
+    else:
+        response = requests.get(url2, auth=auth_details)
+        with open('vertrektijden.xml', 'w',encoding="utf8") as myXMLFile:
+            myXMLFile.write(response.text)
+        vertrekXML = xmltodict.parse(response.text)
+
+        listbox.insert(0,('De volgende treinen vertrekken komend uur vanaf '+keuze+':\n'))
+
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+            eindbestemming = vertrek['EindBestemming']
+            vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrektijd[11:16]
+            ritnummer = vertrek['RitNummer']
+            treintype = vertrek['TreinSoort']
+            spoor = vertrek['VertrekSpoor']
+            try:
+                spoor2=spoor['#text']
+            except KeyError:
+                spoor2='onbekend'
+            try:
+                traject = vertrek['RouteTekst']
+            except KeyError:
+                traject = eindbestemming
+            try:
+                vertraging= vertrek['VertrekVertragingTekst']
+                vertraging2='De vertraging bedraagt: '+vertraging
+            except KeyError:
+                vertraging2=''
+
+            listbox.insert(1,('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:6}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+
+               traject+'.'+'\n'+vertraging2))
+
+        listbox.pack(fill=BOTH)
+
+
+
+button=Button(master=root, text='Launch',command=invoer)
+button.pack(pady=10)
+entry=Entry(master=root)
+entry.pack(padx=10,pady=10)
+scrollbar=Scrollbar(master=root)
+scrollbar.pack(side=RIGHT, fill=Y)
+global listbox
+listbox = Listbox(master=root, yscrollcommand=scrollbar.set)
+label = Label(master=root
+              ,foreground='black',font=('Times New Roman', 24,'bold'),
+              width=18,height=4)
+label.pack()
+
+
+
+
+
+
+
 #######################################################################################################
 #API Variables
 #######################################################################################################
@@ -55,21 +125,35 @@ def station_lijst():
 #########################################################################################################
 def stationskeuze():
     while True:
+        global onbekend
+        onbekend=0
         global keuze
-        keuze=input('Vul gewenste stationsnaam in ')
+
         if keuze in stationslijst:
-            break
+            if keuze in codes:
+                keuze= stationslijst[stationslijst.index(keuze)+3]
+            keuze2=keuze.replace(" " ,"+")
+            keuze3=keuze2.replace("'","%27")
+            keuze4=keuze3.replace(".","%2E")
+            global url2
+            url2= beginurl+keuze4
+            return url2
+        if keuze.upper() in stationslijst:
+            if keuze.upper() in codes:
+                keuze= stationslijst[stationslijst.index((keuze.upper()))+3]
+            keuze2=keuze.replace(" " ,"+")
+            keuze3=keuze2.replace("'","%27")
+            keuze4=keuze3.replace(".","%2E").replace("(","%28").replace(")","%29")
+            global url2
+            url2= beginurl+keuze4
+            return url2
+
         else:
-            print('Station onbekend, probeer het opnieuw.')
-    if keuze in codes:
-        keuze= stationslijst[stationslijst.index(keuze)+3]
-        global afgekort
-        afgekort=1
-    keuze2=keuze.replace(" " ,"+")
-    keuze3=keuze2.replace("'","%27")
-    global url2
-    url2= beginurl+keuze3
-    return url2
+            onbekend=2
+            break
+
+
+
 ##########################################################################################################
 #Real time vertrekinfo voor gekozen station
 ##########################################################################################################
@@ -90,7 +174,10 @@ def treininfo():
         ritnummer = vertrek['RitNummer']
         treintype = vertrek['TreinSoort']
         spoor = vertrek['VertrekSpoor']
-        spoor2=spoor['#text']
+        try:
+            spoor2=spoor['#text']
+        except KeyError:
+            spoor2='onbekend'
         try:
             traject = vertrek['RouteTekst']
         except KeyError:
@@ -101,11 +188,16 @@ def treininfo():
         except KeyError:
             vertraging2=''
 
-        print('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:6}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+ traject+'.'+'\n'+vertraging2)
+        return('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:6}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+
+               traject+'.'+'\n'+vertraging2)
+
+
+
 ############################################################################################################
 #Uit te voeren programma's
 ############################################################################################################
-station_lijst()
-stationskeuze()
-treininfo()
+
+root.mainloop()
+print(onbekend)
+print('ac'.upper() in stationslijst)
 print(url2)
