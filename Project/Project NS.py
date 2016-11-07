b627f3c97a8bb4a924b370d345a82ddfb4c34c18
Tim <stellager@users.noreply.github.com>
2016-11-07 17:59:51 +0100

    Add files via upload
    v1.4

--- Project/Project NS.py
+++ Project/Project NS.py
@@ -1,5 +1,6 @@
 #######################################################################################################
 # writer - Tim Hoogeland
+#
 # imports-
 #######################################################################################################
 import requests
@@ -7,26 +8,81 @@ import xmltodict
 from tkinter import *
 root = Tk()
 
-def invoer():
+scrollbar=Scrollbar(master=root)
+scrollbar.pack(side=RIGHT, fill=Y,expand=False)
+listbox = Listbox(master=root, yscrollcommand=scrollbar.set)
+entry=Entry(master=root)
+entry.pack(padx=10,pady=10)
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
 
-    global keuze
-    listbox.delete(0,END)
-    global keuze
-    keuze=''
-    keuze=entry.get()
-    keuzestring=keuze
-    station_lijst()
-    stationskeuze()
-    if onbekend==2:
-        listbox.insert(0,'Station onbekend, probeer het opnieuw.')
-        listbox.pack(fill=BOTH)
-    else:
-        response = requests.get(url2, auth=auth_details)
+
+auth_details = ('t.hoogeland@live.com','3leOcnJcxNX8Hw3gJ7v5zn3EP2T_R_gEwcotJ-aQ0zzNz4ayWNO-tA')
+stations_url = 'http://webservices.ns.nl/ns-api-stations-v2'
+stations_response = requests.get(stations_url, auth=auth_details)
+stationsXML = xmltodict.parse(stations_response.text)
+beginurl='http://webservices.ns.nl/ns-api-avt?station='
+
+eindbestemming = ''
+vertrektijd=''
+ritnummer=''
+treintype=''
+traject=''
+vertraging=''
+spoor=int
+stationslijst=[]
+codes=[]
+
+with open('stations.xml', 'w',encoding="utf8") as myXMLFile:
+    myXMLFile.write(stations_response.text)
+    myXMLFile.close()
+def stationskeuze():
+    while True:
+        global onbekend
+        onbekend=0
+        global keuze
+
+        if keuze in stationslijst:
+            if keuze in codes:
+                keuze= stationslijst[stationslijst.index(keuze)+3]
+            keuze2=keuze.replace(" " ,"+")
+            keuze3=keuze2.replace("'","%27")
+            keuze4=keuze3.replace(".","%2E")
+            global url2
+            url2= beginurl+keuze4
+            return url2
+        elif keuze.title() in stationslijst:
+            keuze=keuze.title()
+            keuze2=keuze.replace(" " ,"+")
+            keuze3=keuze2.replace("'","%27")
+            keuze4=keuze3.replace(".","%2E")
+            global url2
+            url2= beginurl+keuze4
+            return url2
+        elif keuze.upper() in stationslijst:
+            if keuze.upper() in codes:
+                keuze= stationslijst[stationslijst.index((keuze.upper()))+3]
+            keuze2=keuze.replace(" " ,"+")
+            keuze3=keuze2.replace("'","%27")
+            keuze4=keuze3.replace(".","%2E").replace("(","%28").replace(")","%29")
+            global url2
+            url2= beginurl+keuze4
+            return url2
+
+        else:
+            onbekend=2
+            break
+
+
+def treininfo(x,y):
+        response = requests.get(x, auth=auth_details)
         with open('vertrektijden.xml', 'w',encoding="utf8") as myXMLFile:
             myXMLFile.write(response.text)
+            myXMLFile.close()
         vertrekXML = xmltodict.parse(response.text)
 
-        listbox.insert(0,('De volgende treinen vertrekken komend uur vanaf '+keuze+':\n'))
+
+        listbox.insert(0,('De volgende treinen vertrekken komend uur vanaf '+y+':\n'))
 
         for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
             eindbestemming = vertrek['EindBestemming']
@@ -54,58 +110,10 @@ def invoer():
 
         listbox.pack(fill=BOTH)
 
-
-
-button=Button(master=root, text='Launch',command=invoer)
-button.pack(pady=10)
-entry=Entry(master=root)
-entry.pack(padx=10,pady=10)
-scrollbar=Scrollbar(master=root)
-scrollbar.pack(side=RIGHT, fill=Y)
-global listbox
-listbox = Listbox(master=root, yscrollcommand=scrollbar.set)
-label = Label(master=root
-              ,foreground='black',font=('Times New Roman', 24,'bold'),
-              width=18,height=4)
-label.pack()
-
-
-
-
-
-
-
-#######################################################################################################
-#API Variables
-#######################################################################################################
-auth_details = ('t.hoogeland@live.com','3leOcnJcxNX8Hw3gJ7v5zn3EP2T_R_gEwcotJ-aQ0zzNz4ayWNO-tA')
-stations_url = 'http://webservices.ns.nl/ns-api-stations-v2'
-stations_response = requests.get(stations_url, auth=auth_details)
-stationsXML = xmltodict.parse(stations_response.text)
-beginurl='http://webservices.ns.nl/ns-api-avt?station='
-#######################################################################################################
-#opslag variabelen
-#######################################################################################################
-eindbestemming = ''
-vertrektijd=''
-ritnummer=''
-treintype=''
-traject=''
-vertraging=''
-spoor=int
-stationslijst=[]
-codes=[]
-afgekort=0
-########################################################################################################
-#writers to xml
-########################################################################################################
-with open('stations.xml', 'w',encoding="utf8") as myXMLFile:
-    myXMLFile.write(stations_response.text)
-########################################################################################################
-#Maakt een lijst van alle stations met afkortingen en synoniemen
-########################################################################################################
 def station_lijst():
     for station in stationsXML['Stations']['Station']:
+        if station['Code']=='G':
+            continue
         stationslijst.append(station['Code'])
         codes.append(station['Code'])
         stationslijst.append(station['Namen']['Kort'])
@@ -121,84 +129,46 @@ def station_lijst():
         except TypeError:
             continue
     return stationslijst
-#########################################################################################################
-#User input voor station+creatie van nieuwe url
-#########################################################################################################
-def stationskeuze():
-    while True:
-        global onbekend
-        onbekend=0
-        global keuze
-
-        if keuze in stationslijst:
-            if keuze in codes:
-                keuze= stationslijst[stationslijst.index(keuze)+3]
-            keuze2=keuze.replace(" " ,"+")
-            keuze3=keuze2.replace("'","%27")
-            keuze4=keuze3.replace(".","%2E")
-            global url2
-            url2= beginurl+keuze4
-            return url2
-        elif keuze.upper() in stationslijst:
-            if keuze.upper() in codes:
-                keuze= stationslijst[stationslijst.index((keuze.upper()))+3]
-            keuze2=keuze.replace(" " ,"+")
-            keuze3=keuze2.replace("'","%27")
-            keuze4=keuze3.replace(".","%2E").replace("(","%28").replace(")","%29")
-            global url2
-            url2= beginurl+keuze4
-            return url2
-
-        else:
-            onbekend=2
-            break
 
 
 
-##########################################################################################################
-#Real time vertrekinfo voor gekozen station
-##########################################################################################################
 
+def invoer():
+    global keuze
+    listbox.delete(0,END)
+    global keuze
+    global url2
+    keuze=''
+    keuze=entry.get()
+    keuzestring=keuze
+    stationskeuze()
+    if onbekend==2:
+        listbox.insert(0,'Station onbekend, probeer het opnieuw.')
+        listbox.pack(fill=BOTH)
+    else:
+        treininfo(url2,keuze)
 
-def treininfo():
-    response = requests.get(url2, auth=auth_details)
-    with open('vertrektijden.xml', 'w',encoding="utf8") as myXMLFile:
-        myXMLFile.write(response.text)
-    vertrekXML = xmltodict.parse(response.text)
+def callback(event):
+    g=event
+    invoer()
 
-    print('De volgende treinen vertrekken komend uur vanaf '+keuze+':\n')
-
-    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        eindbestemming = vertrek['EindBestemming']
-        vertrektijd = vertrek['VertrekTijd']
-        vertrektijd = vertrektijd[11:16]
-        ritnummer = vertrek['RitNummer']
-        treintype = vertrek['TreinSoort']
-        spoor = vertrek['VertrekSpoor']
-        try:
-            spoor2=spoor['#text']
-        except KeyError:
-            spoor2='onbekend'
-        try:
-            traject = vertrek['RouteTekst']
-        except KeyError:
-            traject = eindbestemming
-        try:
-            vertraging= vertrek['VertrekVertragingTekst']
-            vertraging2='De vertraging bedraagt: '+vertraging
-        except KeyError:
-            vertraging2=''
-
-        return('Om '+vertrektijd+' vertrekt een '+ '{:9}'.format(treintype)+' met ritnummer '+'{:6}'.format(ritnummer)+' naar '+ eindbestemming+' vanaf spoor: '+'{:2}'.format(spoor2)+' ,de stations op dit traject zijn: '+
-               traject+'.'+'\n'+vertraging2)
+button=Button(master=root, text='Zoek',command=invoer)
+button.pack(pady=10)
 
 
 
-############################################################################################################
-#Uit te voeren programma's
-############################################################################################################
+label = Label(master=root
+              ,foreground='black',font=('Times New Roman', 24,'bold'),
+              width=18,height=4)
 
+label.pack()
+entry.bind("<Return>",callback)
+
+
+
+###########################
+station_lijst()
+treininfo('http://webservices.ns.nl/ns-api-avt?station=Haarlem','Haarlem')
 root.mainloop()
-print(onbekend)
-print('ac'.upper() in stationslijst)
+
 print(url2)
